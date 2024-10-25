6e6007129617eccc3f0f763e1320076ade334337
Scott Starker <Scott.Starker@protonmail.com>
2024-10-25 12:18:47 -0700

    update promotion page

--- promotionMaterials/promotion.php
+++ promotionMaterials/promotion.php
@@ -1,138 +1,166 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta http-equiv="Content-Type"		content="text/html; charset=utf-8" />
+	<meta name="viewport"				content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<title>Promotion Materials for ScriptureEarth.org</title>
-	<meta name="ObjectType" content="Document" />
-	<meta http-equiv="Window-target" content="_top" />
-	<meta name="Created-by" content="Scott Starker" />
-	<meta name="Updated-by" content="Scott Starker" />
-	<meta name="Maintained-by" content="Website" />
-	<meta name="Approved-by" content="Bill Dyck, Access Coordinator" />
+	<meta name="ObjectType"				content="Document" />
+	<meta http-equiv="Window-target"	content="_top" />
+	<meta name="Created-by"				content="Scott Starker" />
+	<meta name="Updated-by"				content="Scott Starker" />
+	<meta name="Maintained-by"			content="Website" />
+	<meta name="Approved-by"			content="Bill Dyck, Access Coordinator" />
 	<style type="text/css">
-		@media only screen and (max-width: 480px) {
+		@media only screen and (max-width: 489px) {
 			/* (max-width: 412px) for Samsung S8+ 2/20/2019 */
+		}
+		@media only screen and (max-width: 989px) {
 			div.contacts {
 				float: none;
 				width: 100%;
+				margin-left: 0;
+				margin-right: 0;
+				text-align: center;
+			}
+			div.clearfix {
+				clear: both;
+				display: block;
+			}
+			img, video {
+				width: 450px;
+				height: auto;
 			}
 		}
 
-		@media only screen and (min-width: 481px) {
+		@media only screen and (min-width: 990px) {
+			* {
+				box-sizing: border-box;
+			}
+
 			div.contacts {
 				float: left;
 				width: 46%;
+				margin-left: 20px;
+				margin-right: 20px;
+				margin-top: 10px;
+				margin-bottom: 14px;
+			}
+			/*div.contacts {
+				/*margin-left: 5px;* /
+				/*centers the screen * /
+				margin-left: auto;
+				margin-right: auto;
+			}*/
+
+			div.clearfix {
+				clear: both;
+				text-align: center;
+				/* the next two line are what makes 'contacts' bottom vertical! */
+				display: flex;
+				/* defines flexbox */
+				align-items: flex-end;
+				/* bottom of the box */
+			}
+
+			img, video {
+				width: 500px;
+				height: auto;
 			}
 		}
 
-		* {
-			box-sizing: border-box;
-		}
-
 		body {
-			font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-			font-size: 140%;
+			/*font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;*/
+			font-family: Chivo, 'Gill Sans', Tahoma, Geneva, Verdana, sans-serif;
+			font-size: 120%;
 			width: 100%;
-		}
-
-		div.contacts {
-			margin-left: 5px;
-			margin-right: 5px;
-			margin-top: 10px;
-			margin-bottom: 14px;
-			/* centers the screen */
-			margin-left: auto;
-			margin-right: auto;
-		}
-
-		div.clearfix {
-			text-align: center;
-			/* the next two line are what makes 'contacts' bottom vertical! */
-			display: flex;
-			/* defines flexbox */
-			align-items: flex-end;
-			/* bottom of the box */
-		}
-
-		div.clearfix::after {
-			content: "";
-			clear: both;
-			display: table;
+			padding: 0;
+			margin: 0;
 		}
 
 		figure {
+			text-align: center;
 			margin-top: 4px;
+		}
+
+		h2 {
+			color: #980000;
+			font-size: 17pt;
+			margin: 20px 40px;
+		}
+		h3, p {
+			margin: 20px 40px;
+		}
+		h3 {
+			color: red;
+			font-size: 110%;
 		}
 	</style>
 </head>
 <body>
 	<h1 style='text-align: center; '>Promotion Materials for ScriptureEarth.org</h1>
 
-	<h3 style='margin-left: auto; margin-right: auto; width: 720px; '>Note: Position the curson over the image and right-click to see the actions including "Save Video (or Image) As..."</h3>
+	<h3 style='text-align: center; '>Note: Position the curson over the image and right-click to see the actions including "Save Video (or Image) As..."</h3>
 
-	<h2 style='text-align: center; margin-bottom: 40px; '>English Items</h2>
+	<h2 style='text-align: center; margin-top: 40px; margin-bottom: 40px; '>English Items</h2>
 
-	<h3 style='text-align: center; margin-bottom: 40px; font-weight: normal; '>One of the promotion materials: <a href="https://outreach.ca/article/ArticleId/3937/Scripture-Earth-One-of-the-largest-repositories-of-Bible-resources-available" target="_blank">A blog from Deb &amp; Ramón Padilla</a></h3>
+	<h3 style='color: darkgreen; text-align: center; margin-bottom: 40px; font-weight: normal; '>One of the promotion materials: <a href="https://outreach.ca/article/ArticleId/3937/Scripture-Earth-One-of-the-largest-repositories-of-Bible-resources-available" target="_blank">A blog from Deb &amp; Ramón Padilla</a></h3>
 	
 	<div class='clearfix'>
 		<div class='contacts'>
-			<video style="width: 500px; height: auto; " poster='JimMcCabe-picture.png' controls>
+			<video poster='JimMcCabe-picture.png' controls>
 				<source src="ScriptureEarth.org.mp4" type="video/mp4">
 				Your browser does not support the video tag.
 			</video>
 			<figure>Jim McCabe - 3 minute video</figure>
 		</div>
 		<div class='contacts'>
-			<img src='SE.org%20card%20-%20globe%20-%20QR.jpg' alt='ScriptureEarth.org card - globe - QR' style='width: 500px; height: auto; ' />
+			<img src='SE.org%20card%20-%20globe%20-%20QR.jpg' alt='ScriptureEarth.org card - globe - QR' />
 			<figure>ScriptureEarth.org card - globe - QR</figure>
 		</div>
 	</div>
 
 	<div class='clearfix'>
 		<div class='contacts'>
-			<img src='SE.org%20card%20-%20globe.jpg' alt='ScriptureEarth.org card - globe' style='width: 500px; height: auto; ' />
+			<img src='SE.org%20card%20-%20globe.jpg' alt='ScriptureEarth.org card - globe' />
 			<figure>ScriptureEarth.org card - globe</figure>
 		</div>
 		<div class='contacts'>
-			<img src='SE.org%20card%20-%20LUMO.jpg' alt='ScriptureEarth.org card - LUMO' style='width: 500px; height: auto; ' />
+			<img src='SE.org%20card%20-%20LUMO.jpg' alt='ScriptureEarth.org card - LUMO' />
 			<figure>ScriptureEarth.org card - LUMO</figure>
 		</div>
 	</div>
 
 	<div class='clearfix'>
 		<div class='contacts'>
-			<img src='SE.org%20card%20-%20Share%20it%20-%20front.jpg' alt='SE.org card from Deb &amp; Ramón Padilla - front' style='width: 500px; height: auto; ' />
+			<img src='SE.org%20card%20-%20Share%20it%20-%20front.jpg' alt='SE.org card from Deb &amp; Ramón Padilla - front' />
 			<figure>SE.org card from Deb &amp; Ramón Padilla - front</figure>
 		</div>
 		<div class='contacts'>
-			<img src='SE.org%20card%20-%20Share%20it%20-%20back.jpg' alt='SE.org card from Deb &amp; Ramón Padilla - back' style='width: 500px; height: auto; ' />
+			<img src='SE.org%20card%20-%20Share%20it%20-%20back.jpg' alt='SE.org card from Deb &amp; Ramón Padilla - back' />
 			<figure>SE.org card from Deb &amp; Ramón Padilla - back</figure>
 		</div>
 	</div>
 
 	<div class='clearfix'>
 		<div class='contacts'>
-			<img src='SE.org - man poster - take cards.jpg' alt='ScriptureEarth.org - man poster - take cards' style='width: 500px; height: auto; ' />
+			<img src='SE.org - man poster - take cards.jpg' alt='ScriptureEarth.org - man poster - take cards' />
 			<figure>ScriptureEarth.org - man poster - take cards</figure>
 		</div>
 		<div class='contacts'>
-			<img src='SE.org - women poster - QR.jpg' alt='ScriptureEarth.org - women poster - QR' style='width: 500px; height: auto; ' />
+			<img src='SE.org - women poster - QR.jpg' alt='ScriptureEarth.org - women poster - QR' />
 			<figure>ScriptureEarth.org - women poster - QR</figure>
 		</div>
 	</div>
 
 	<div class='clearfix'>
-		<p>
 		<div class='contacts'>
-			<img src='SE.org - women poster - take cards.jpg' alt='ScriptureEarth.org - women poster - take cards' style='width: 500px; height: auto; ' />
+			<img src='SE.org - women poster - take cards.jpg' alt='ScriptureEarth.org - women poster - take cards' />
 			<figure>ScriptureEarth.org - women poster - take cards</figire>
 		</div>
 		<div class='contacts'>
-			<img src='SE.org%20-%20QR.png' alt='ScriptureEarth.org - QR' style='width: 500px; height: auto; ' />
+			<img src='SE.org%20-%20QR.png' alt='ScriptureEarth.org - QR' />
 			<figure>ScriptureEarth.org - QR</figure>
 		</div>
-		</p>
 	</div>
 
 	<hr style="width: 80%; margin-top: 10px; margin-bottom: 50px; ">
@@ -141,11 +169,11 @@
 
 	<div class='clearfix'>
 		<div class='contacts'>
-			<img src='Tarjeta-FRENTE.jpg' alt='Tarjeta-FRENTE' style='width: 500px; height: auto; ' />
+			<img src='Tarjeta-FRENTE.jpg' alt='Tarjeta-FRENTE' />
 			<figure>Tarjeta - FRENTE</figure>
 		</div>
 		<div class='contacts'>
-			<img src='Tarjeta-REVERSO.jpg' alt='Tarjeta-REVERSO' style='width: 500px; height: auto; ' />
+			<img src='Tarjeta-REVERSO.jpg' alt='Tarjeta-REVERSO' />
 			<figure>Tarjeta - REVERSO</figure>
 		</div>
 	</div>
